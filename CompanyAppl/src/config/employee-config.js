1f4b5953baf7ba3b18ae2908f94eb253b226468d
MatveyRomodanovskiy <startpolus@gmail.com>
2023-02-02 23:08:10 +0300

    final

--- CompanyAppl/src/config/employee-config.js
+++
@@ -1,8 +0,0 @@
-export const employeeConfig = {
-    minId: 100000000,
-    maxId: 999999999,
-    countries: {
-        "Israel": ["Lod", "Tel Aviv", "Rehovot"],
-        "USA": ["New York", "Boston", "Vashington"]
-    }
-}
