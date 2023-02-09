dea89b39e6a6686ea01751932e229139cea923e6
MatveyRomodanovskiy <startpolus@gmail.com>
2023-02-09 17:59:06 +0300

    with css

--- PlayerAppl/src/main.js
+++ PlayerAppl/src/main.js
@@ -2,7 +2,7 @@ import { DataForm } from "./ui/input-data-form.js";
 import { VideoPlayer } from "./ui/video-player.js";
 import { sleep } from "./utils/sleep.js";
 const inputForm = new DataForm('form-section');
-const video = new VideoPlayer("video-section>")
+const video = new VideoPlayer("video-section")
 
 const TIMEOUT = 10000;
 // const promise = sleep(TIMEOUT);
